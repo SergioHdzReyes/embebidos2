6621fb9f0a979ab3e865cfb43fc02074f56518b5
SergioHdzReyes <sergio.hdzreyes@gmail.com>
2017-04-19 09:06:40 -0500

    Function onDeleted

--- app/src/main/java/com/example/checo/embebidosii/MainActivity.java
+++ app/src/main/java/com/example/checo/embebidosii/MainActivity.java
@@ -9,4 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    @Override
+    protected void onDeleted() {
+        
+    }
 }
